c682eb33317e251deab784e294c7634c27e6080f
OgNeto <ogsilvanetu@gmail.com>
2025-06-11 20:45:28 -0300

    Adding the teacher repository

--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Teacher } from './entities/teacher.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TeachersService {
+  constructor (
+    @InjectRepository(Teacher)
+    private readonly teacherRepository: Repository<Teacher>,
+  ) {}
+
   create(createTeacherDto: CreateTeacherDto) {
     return 'This action adds a new teacher';
   }
