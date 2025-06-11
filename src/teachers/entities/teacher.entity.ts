291cbf381fb9286cf1dd5f3d8d473a4da2dd8202
OgNeto <ogsilvanetu@gmail.com>
2025-06-11 20:39:20 -0300

    Connecting the teachers entity to the database

--- src/teachers/entities/teacher.entity.ts
+++ src/teachers/entities/teacher.entity.ts
@@ -1,1 +1,23 @@
-export class Teacher {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Teacher {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'varchar', length: 50 })
+    teacher_name: string;
+
+    @Column({ type: 'varchar', unique: true, length:50 })
+    teacher_email: string;
+
+    @Column({ type: 'varchar',  length: 20 })
+    teacher_phoneNumber: string;
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updaetdAt: string;
+}
