e634a0b9a5bf364deee9e06e188c895e392acd0a
OgNeto <ogsilvanetu@gmail.com>
2025-06-11 20:26:24 -0300

    Adding the database settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT') ?? '5432', 10),
+        database: config.get('DB_NAME'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        synchronize: true, //Just to study.
+        autoLoadEntities: true, //Just to study.
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
