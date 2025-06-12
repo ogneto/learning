dcbe810acbda409b674bbb2a383c1d1a40eb0d82
OgNeto <ogsilvanetu@gmail.com>
2025-06-11 21:06:51 -0300

    Updating the teacher dto

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTeacherDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateTeacherDto {
+
+    @IsString()
+    @MaxLength(50)
+    @MinLength(4)
+    teacher_name: string;
+
+    @IsEmail()
+    @MaxLength(50)
+    @MinLength(10)
+    teacher_email: string;
+
+    @IsString()
+    @MaxLength(20)
+    @MinLength(10)
+    teacher_phoneNumber: string;
+}
