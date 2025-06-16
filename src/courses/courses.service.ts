c6ac3994aa74ad4db788b275c627ca8ff6ec4c97
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 14:42:03 -0300

    Injecting course repository

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './entities/course.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
+
+  constructor (
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {};
+
+
   create(createCourseDto: CreateCourseDto) {
     return 'This action adds a new course';
   }
