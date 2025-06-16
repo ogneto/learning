e94e7e0b28b328b1ed1971b7a5dc0aa83b7c067c
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 15:03:18 -0300

    Updating createCourseDto

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCourseDto {}
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
+  @MaxLength(20)
+  @MinLength(5)
+  course_name: string;
+
+  @IsString()
+  @MaxLength(50)
+  @MinLength(10)
+  course_description: string;
+}
