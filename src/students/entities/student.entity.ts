54e1eff1a6787ad49bf272ab4718d53f0612851a
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 11:26:47 -0300

    Add student entity

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,1 +1,28 @@
-export class Student {}
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Student {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'varchar', length: 50 })
+    student_name: string;
+
+    @Column({ type: 'varchar', length: 50, unique: true })
+    student_email: string;
+
+    @Column({ type: 'varchar', length: 12, unique: true })
+    student_registration: string;
+
+    @BeforeInsert()
+    generateRegistration() {
+        this.student_registration = Array.from({length: 12}, () => Math.floor(Math.random() * 10)).join('');
+    }
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updatedAt: string;
+}
