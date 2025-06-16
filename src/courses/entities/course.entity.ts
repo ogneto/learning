defaca9fb5f7a4872e36407ce5a12c09905f6398
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 14:38:08 -0300

    updating course entity

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,1 +1,25 @@
-export class Course {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Course {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true, length: 20 })
+  course_name: string;
+
+  @Column({ type: 'varchar', length:  50 })
+  course_description: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
