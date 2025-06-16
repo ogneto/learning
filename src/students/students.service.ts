f6b802a14cdff42b33dfddd6c9f88d737848ae40
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 11:34:59 -0300

    Add student repository on student service

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StudentsService {
+
+  constructor(
+  @InjectRepository(Student)
+  private readonly studentRepository: Repository<Student>,
+  ) {}
+  
   create(createStudentDto: CreateStudentDto) {
     return 'This action adds a new student';
   }
