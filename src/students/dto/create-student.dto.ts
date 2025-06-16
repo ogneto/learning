28fb11526038ac73a84fcfbebf87fa4b4e9ea45c
OgNeto <ogsilvanetu@gmail.com>
2025-06-16 12:01:09 -0300

    Updating the create student dto

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,14 @@
-export class CreateStudentDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateStudentDto {
+
+    @IsString()
+    @MaxLength(50)
+    @MinLength(4)
+    student_name: string;
+
+    @IsEmail()
+    @MaxLength(50)
+    @MinLength(10)
+    student_email: string;
+}
